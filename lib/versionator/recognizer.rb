6c008509be61162e23b2cc2504936a484e58e2f7
Riyad Preukschas <riyad@informatik.uni-bremen.de>
2011-01-20 00:01:01 +0100

    Added general recognizer class.

---
+++ lib/versionator/recognizer.rb
@@ -0,0 +1,25 @@
+
+require 'active_support'
+include ActiveSupport::Inflector
+
+require File.expand_path("recognizer/base", File.dirname(__FILE__))
+
+module Versionator
+  module Recognizer
+    def self.all
+      recognizers = []
+
+      # find all recognizer scripts
+      Dir.glob(File.dirname(__FILE__) + '/recognizer/*_recognizer.rb') do |rec_file|
+        # require script
+        require rec_file
+
+        # add recognizer class to list
+        rec_name = 'versionator/'+rec_file[(File.dirname(__FILE__)).size+1..-4]
+        recognizers << rec_name.camelize.constantize
+      end
+
+      recognizers
+    end
+  end
+end
