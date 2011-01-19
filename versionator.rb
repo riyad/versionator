27f9349d54d79458447a983d5049d36455678ca3
Riyad Preukschas <riyad@informatik.uni-bremen.de>
2011-01-20 00:08:33 +0100

    Added sinatra app.

---
+++ versionator.rb
@@ -0,0 +1,44 @@
+#!/usr/bin/env ruby
+
+require 'sinatra/base'
+
+require 'haml'
+require 'sinatra/reloader'
+
+require File.expand_path('lib/versionator', File.dirname(__FILE__))
+
+module Versionator
+  class Application < Sinatra::Base
+    configure(:development) do
+      register Sinatra::Reloader
+      also_reload ["*.rb", "lib/**/*.rb"]
+    end
+
+    helpers do
+      def recognizers
+        Versionator::Recognizer.all
+      end
+
+      def dirs
+        File.readlines('dirs').map { |dir| dir.chomp }
+      end
+    end
+
+    get '/' do
+      @dirs = dirs
+      @recognizers = recognizers
+      @wares = []
+      @dirs.each do |dir|
+        @recognizers.each do |rec_class|
+          rec = rec_class.new(dir)
+          @wares << rec if rec.detected?
+        end
+      end
+
+      haml :index
+    end
+
+    # start the server if ruby file executed directly
+    run! #if app_file == $0
+  end
+end
