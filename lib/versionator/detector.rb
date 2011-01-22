8ee9e0d0b05db010b5c89a9e451719a845d3a04c
Riyad Preukschas <riyad@informatik.uni-bremen.de>
2011-01-22 20:33:24 +0100

    Fixed detector loading.

--- lib/versionator/detector.rb
+++ lib/versionator/detector.rb
@@ -9,12 +9,12 @@ module Versionator
     def self.all
       detectors = []
 
-      # find all recognizer scripts
-      Dir.glob(File.dirname(__FILE__) + '/detector/*_detector.rb') do |det_file|
+      # find all detector scripts
+      Dir.glob(File.expand_path('../detector/*_detector.rb', __FILE__)) do |det_file|
         # require script
         require det_file
 
-        # add recognizer class to list
+        # add detector class to list
         det_name = 'versionator/'+det_file[(File.dirname(__FILE__)).size+1..-4]
         detectors << det_name.camelize.constantize
       end
