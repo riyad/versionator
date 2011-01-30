493e39fc51196469195636f21a66d75d0ab6d1b2
Riyad Preukschas <riyad@informatik.uni-bremen.de>
2011-01-30 22:50:47 +0100

    Added Piwik detector.

---
+++ lib/versionator/detector/piwik_detector.rb
@@ -0,0 +1,24 @@
+
+module Versionator
+  module Detector
+    class PiwikDetector < Base
+      set :basic_name, "piwik"
+      set :app_name, "Piwik"
+      set :project_url, "http://piwik.org"
+
+      set :detect_dirs, %w{config core js libs misc plugins themes}
+      set :detect_files, %w{core/Version.php index.php piwik.php}
+
+      set :installed_version_file, "core/Version.php"
+      set :installed_version_regexp, /^\sconst VERSION = '(.+)';$/
+
+      set :newest_version_url, 'http://piwik.org/'
+      set :newest_version_selector, '#sidebar a.download-button strong'
+      set :newest_version_regexp, /^Download Piwik (.+)\s*$/
+
+      def project_url_for_version(version)
+        "#{project_url}/blog/piwik-#{version.to_s.gsub('.','-')}/"
+      end
+    end
+  end
+end
