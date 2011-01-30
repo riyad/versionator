713a17efd3b9d0d4567f8cdba0bd437a91c0c5df
Riyad Preukschas <riyad@informatik.uni-bremen.de>
2011-01-30 22:29:36 +0100

    Added Roundcube detector.

---
+++ lib/versionator/detector/roundcube_detector.rb
@@ -0,0 +1,28 @@
+
+module Versionator
+  module Detector
+    class RoundcubeDetector < Base
+      set :basic_name, "roundcube"
+      set :app_name, "Roundcube"
+      set :project_url, "http://roundcube.net/"
+
+      set :detect_dirs, %w{bin config installer plugins program skins}
+      set :detect_files, %w{index.php}
+
+      set :installed_version_file, "index.php"
+      set :installed_version_regexp, /^ \| Version ([^ ]+)\s+\|$/
+
+      set :newest_version_url, 'http://roundcube.net/'
+      set :newest_version_selector, '#main a.downloadButton'
+      set :newest_version_regexp, /^Version (.+) \(.+\)$/
+
+      def project_url_for_version(version)
+        if version >= Versionomy.parse('0.5')
+          "http://sourceforge.net/projects/roundcubemail/files/roundcubemail/#{version}/README_#{version}.txt/view"
+        else
+          "http://sourceforge.net/projects/roundcubemail/files/roundcubemail/#{version}/release_notes_#{version}.txt/view"
+        end
+      end
+    end
+  end
+end
