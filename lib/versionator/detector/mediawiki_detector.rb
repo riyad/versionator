614ea1ceeb81e699aa115d8076221de623c962e6
Riyad Preukschas <riyad@informatik.uni-bremen.de>
2011-01-30 20:32:41 +0100

    Added MediawikiDetector.

---
+++ lib/versionator/detector/mediawiki_detector.rb
@@ -0,0 +1,32 @@
+
+module Versionator
+  module Detector
+    class MediawikiDetector < Base
+      set :basic_name, "mediawiki"
+      set :app_name, "MediaWiki"
+      set :project_url, "http://www.mediawiki.org"
+
+      set :detect_dirs, %w{bin config extensions images includes maintenance skins}
+      set :detect_files, %w{api.php index.php redirect.php RELEASE-NOTES thumb.php}
+
+      set :installed_version_file, "RELEASE-NOTES"
+      set :installed_version_regexp, /^== MediaWiki (.+) ==$/
+
+      set :newest_version_url, 'http://www.mediawiki.org/wiki/Download'
+      set :newest_version_selector, '#bodyContent .plainlinks a'
+      set :newest_version_regexp, /^Download MediaWiki (.+)$/
+
+      def contents_detected?
+        true if find_first_line(:matching => installed_version_regexp, :in_file => File.join(base_dir, installed_version_file))
+      end
+
+      def project_url_for_installed_version
+        "http://svn.wikimedia.org/svnroot/mediawiki/tags/REL#{installed_version.to_s.gsub('.', '_')}/phase3/RELEASE-NOTES"
+      end
+
+      def project_url_for_newest_version
+        "http://svn.wikimedia.org/svnroot/mediawiki/tags/REL#{newest_version.to_s.gsub('.', '_')}/phase3/RELEASE-NOTES"
+      end
+    end
+  end
+end
