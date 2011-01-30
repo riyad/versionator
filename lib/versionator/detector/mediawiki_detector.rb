8148c944a86e6e7025b99949139523c54dbf8ffa
Riyad Preukschas <riyad@informatik.uni-bremen.de>
2011-01-31 00:16:23 +0100

    Simplified MediaWiki detector.

--- lib/versionator/detector/mediawiki_detector.rb
+++ lib/versionator/detector/mediawiki_detector.rb
@@ -16,10 +16,6 @@ module Versionator
       set :newest_version_selector, '#bodyContent .plainlinks a'
       set :newest_version_regexp, /^Download MediaWiki (.+)$/
 
-      def contents_detected?
-        true if find_first_line(:matching => installed_version_regexp, :in_file => installed_version_file)
-      end
-
       def project_url_for_version(version)
         "http://svn.wikimedia.org/svnroot/mediawiki/tags/REL#{version.to_s.gsub('.', '_')}/phase3/RELEASE-NOTES"
       end
