0eefb12de288b52e4eb86a2e5fedb1dc942a4dd1
Riyad Preukschas <riyad@informatik.uni-bremen.de>
2011-02-02 14:03:04 +0100

    Updated detector loading.

--- lib/versionator/detector.rb
+++ lib/versionator/detector.rb
@@ -2,7 +2,7 @@
 require 'active_support'
 include ActiveSupport::Inflector
 
-require File.expand_path("../detector/base", __FILE__)
+require 'versionator/detector/base'
 
 module Versionator
   module Detector
@@ -10,13 +10,13 @@ module Versionator
       detectors = []
 
       # find all detector scripts
-      Dir.glob(File.expand_path('../detector/*_detector.rb', __FILE__)) do |det_file|
+      Dir.glob(File.expand_path('../detector/*.rb', __FILE__)) do |det_file|
         # require script
         require det_file
 
         # add detector class to list
         det_name = 'versionator/'+det_file[(File.dirname(__FILE__)).size+1..-4]
-        detectors << det_name.camelize.constantize
+        detectors << det_name.camelize.constantize unless det_name.end_with?("/base")
       end
 
       detectors
