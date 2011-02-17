e9e52a4d21aa0a6efe407e6bc10212d74a94bbd9
Riyad Preukschas <riyad@informatik.uni-bremen.de>
2011-02-17 22:47:03 +0100

    Removed link_to_app_release helper.

--- lib/versionator/application.rb
+++ lib/versionator/application.rb
@@ -95,12 +95,6 @@ module Versionator
         link_to_external("#{mini_logo_for(app)} #{app.app_name} Website", app.project_url)
       end
 
-      def link_to_app_release(app, version)
-        version_method = version.to_sym
-        url_method = "project_url_for_#{version.to_s}".to_sym
-        link_to_external("#{image("link")} #{app.send(version_method)} Release", app.send(url_method), :class => version_method.to_s)
-      end
-
       def link_to_external(text, href, html_options = {})
         %Q{<a href="#{href}" #{%Q{id="#{html_options[:id]}"} if html_options[:id]} #{%Q{class="#{html_options[:class]}"} if html_options[:class]}>} +
           text +
