59732975db930d29024f2c773cfc6a8301cf8074
Riyad Preukschas <riyad@informatik.uni-bremen.de>
2011-11-20 17:26:27 +0100

    Fix GitLab detector to also detect the development version

--- lib/versionator/detector/gitlab.rb
+++ lib/versionator/detector/gitlab.rb
@@ -7,10 +7,10 @@ module Versionator
       set :project_url, "http://gitlabhq.com"
 
       set :detect_dirs, %w{app config lib public vendor}
-      set :detect_files, %w{CHANGELOG Rakefile README.rdoc}
+      set :detect_files, %w{CHANGELOG Rakefile VERSION}
 
-      set :installed_version_file, "CHANGELOG"
-      set :installed_version_regexp, /^v (.+)$/
+      set :installed_version_file, "VERSION"
+      set :installed_version_regexp, /^(.+)$/
 
       set :newest_version_url, 'http://gitlabhq.com/releases.html'
       set :newest_version_selector, 'h2'
@@ -18,7 +18,7 @@ module Versionator
 
       # Overridden to make sure we detect GitLab and not just any RoR app.
       def contents_detected?
-        true if find_first_line(:matching => /GitLab/, :in_file => "README.rdoc")
+        true if find_first_line(:matching => /Gitlab/, :in_file => "Rakefile")
       end
 
       def project_url_for_version(version)
