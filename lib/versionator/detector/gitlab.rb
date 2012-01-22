91e1adabaa3b718e75205ac04269f2ce86f0ab5a
Riyad Preukschas <riyad@informatik.uni-bremen.de>
2012-01-22 21:48:18 +0100

    Fix Gitlab detector

--- lib/versionator/detector/gitlab.rb
+++ lib/versionator/detector/gitlab.rb
@@ -12,17 +12,13 @@ module Versionator
       set :installed_version_file, "VERSION"
       set :installed_version_regexp, /^(.+)$/
 
-      set :newest_version_url, 'http://gitlabhq.com/releases.html'
-      set :newest_version_selector, 'h2'
-      set :newest_version_regexp, /^Latest version is v(.+) \'.*\'/
+      set :newest_version_url, 'http://gitlabhq.com/'
+      set :newest_version_selector, 'h4'
+      set :newest_version_regexp, /^Gitlab (.+) released\./
 
-      # Overridden to make sure we detect GitLab and not just any RoR app.
+      # Overridden to make sure we detect Gitlab and not just any RoR app.
       def contents_detected?
         true if find_first_line(:matching => /Gitlab/, :in_file => "Rakefile")
-      end
-
-      def project_url_for_version(version)
-        "http://gitlabhq.com/releases.html"
       end
     end
   end
