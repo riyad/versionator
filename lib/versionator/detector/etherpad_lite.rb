b61a5354f2b3e51627f8c653bf4444cba2712e64
Riyad Preukschas <riyad@informatik.uni-bremen.de>
2012-05-04 00:55:30 +0200

    Add Etherpad Lite detector

---
+++ lib/versionator/detector/etherpad_lite.rb
@@ -0,0 +1,24 @@
+
+module Versionator
+  module Detector
+    # Detects {Etherpad Lite}[http://etherpad.org/].
+    class EtherpadLite < Base
+      set :app_name, "Etherpad Lite"
+      set :project_url, "http://etherpad.org/"
+
+      set :detect_dirs, %w{bin doc node static}
+      set :detect_files, %w{CHANGELOG.md package.json README.md}
+
+      set :installed_version_file, "CHANGELOG.md"
+      set :installed_version_regexp, /^# v (.+)$/
+
+      set :newest_version_url, 'https://github.com/Pita/etherpad-lite/blob/master/CHANGELOG.md'
+      set :newest_version_selector, '#files .blob article h1'
+      set :newest_version_regexp, /^v (.+)$/
+
+      def project_url_for_version(version)
+         "https://github.com/Pita/etherpad-lite"
+      end
+    end
+  end
+end
