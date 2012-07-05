14a907c358d104e0b952d5d62844187c0333bf58
Riyad Preukschas <riyad@informatik.uni-bremen.de>
2012-07-05 20:28:53 +0200

    Redo errors

--- lib/versionator/application.rb
+++ lib/versionator/application.rb
@@ -124,11 +124,11 @@ module Versionator
       # The subject must have +.head+ and +.body+ decendants.
       # You can also specify the initial state of the subject element using the _initial_subj_state_ param with either the +:collapsed+ or +:expanded+ values.
       def make_collapsable(subj_selector, initial_subj_state = :collapsed)
-        %Q{<div class="collapsable-button collapsable-indicator">} +
+        %Q{<div class="collapsable-button collapsable-indicator pull-left">} +
           image("container-expand", :class => "collapsable-button-expand") +
           image("container-collapse", :class => "collapsable-button-collapse") +
-          %Q{<script>
-            $(document).ready(function() {
+          %Q{<script defer="defer">
+            $(function() {
               setupCollapsableContainer("#{subj_selector}", "#{initial_subj_state}");
             });
         </script>} +
