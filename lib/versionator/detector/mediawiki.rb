cc6561ed32621246fe99098b1f905d6ddabf274c
Riyad Preukschas <riyad@informatik.uni-bremen.de>
2012-08-13 01:54:15 +0200

    Update MediaWiki detector

--- lib/versionator/detector/mediawiki.rb
+++ lib/versionator/detector/mediawiki.rb
@@ -7,25 +7,14 @@ module Versionator
       set :project_url, "http://www.mediawiki.org"
 
       set :detect_dirs, %w{bin extensions images includes maintenance skins}
-      set :detect_files, %w{api.php index.php redirect.php thumb.php}
+      set :detect_files, %w{api.php includes/DefaultSettings.php index.php redirect.php thumb.php}
 
-      set :installed_version_file, "RELEASE-NOTES" # see contents_detected?
-      set :installed_version_regexp, /^== MediaWiki (.+) ==$/
+      set :installed_version_file, "includes/DefaultSettings.php" # see contents_detected?
+      set :installed_version_regexp, /^\$wgVersion\s*= '(.+)';$/
 
       set :newest_version_url, 'http://www.mediawiki.org/wiki/Download'
       set :newest_version_selector, '#bodyContent .plainlinks a'
       set :newest_version_regexp, /^Download MediaWiki (.+)$/
-
-      # Overriden to detect RELEASE-NOTES-x.yy files form 1.18 onwards
-      def contents_detected?
-        # look for RELEASE-NOTES or RELEASE-NOTES-x.yy files and sort them,
-        # because we want the latest one in case there are multiple
-        release_notes = Dir.glob(File.expand_path("RELEASE-NOTES*", base_dir)).sort
-        # the paths have been expanded, make them relative again
-        @@installed_version_file = release_notes.last[File.expand_path(base_dir).size..-1]
-
-        !release_notes.empty?
-      end
 
       def project_url_for_version(version)
         if version < Versionomy.parse('1.18')
