0a1201f7b4f9b73f646ab5e40846d885d6c6247c
Riyad Preukschas <riyad@informatik.uni-bremen.de>
2012-08-23 22:01:05 +0200

    Update phpBB detector

--- lib/versionator/detector/phpbb.rb
+++ lib/versionator/detector/phpbb.rb
@@ -4,7 +4,7 @@ module Versionator
     # Detects {phpBB}[http://www.phpbb.com].
     class Phpbb < Base
       set :app_name, "phpBB"
-      set :project_url, "http://www.phpbb.com"
+      set :project_url, "https://www.phpbb.com/"
 
       set :detect_dirs, %w{adm download files includes install store styles}
       set :detect_files, %w{common.php cron.php faq.php index.php install/schemas/schema_data.sql posting.php search.php viewforum.php viewonline.php viewtopic.php}
@@ -12,7 +12,7 @@ module Versionator
       set :installed_version_file, "install/schemas/schema_data.sql"
       set :installed_version_regexp, /^INSERT INTO .+ VALUES \('version', '(.+)'\);$/
 
-      set :newest_version_url, 'http://www.phpbb.com/downloads/'
+      set :newest_version_url, 'https://www.phpbb.com/downloads/'
       set :newest_version_selector, '#full-package .download-container a'
       set :newest_version_regexp, /^Latest stable phpBB: ([\d\.]+)/
 
