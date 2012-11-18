bd57de65337d4cad584eaa2e0f52ee35aeda00e6
Riyad Preukschas <riyad@informatik.uni-bremen.de>
2012-11-18 20:55:50 +0100

    Update EtherpadLite detector

--- lib/versionator/detector/etherpad_lite.rb
+++ lib/versionator/detector/etherpad_lite.rb
@@ -1,4 +1,3 @@
-
 module Versionator
   module Detector
     # Detects {Etherpad Lite}[http://etherpad.org/].
@@ -6,18 +5,18 @@ module Versionator
       set :app_name, "Etherpad Lite"
       set :project_url, "http://etherpad.org/"
 
-      set :detect_dirs, %w{bin doc src static}
+      set :detect_dirs, %w{bin doc src tools var}
       set :detect_files, %w{CHANGELOG.md README.md settings.json.template}
 
       set :installed_version_file, "CHANGELOG.md"
       set :installed_version_regexp, /^# v\s*(.+)$/
 
-      set :newest_version_url, 'https://github.com/Pita/etherpad-lite/blob/master/CHANGELOG.md'
-      set :newest_version_selector, '#files .blob article h1'
-      set :newest_version_regexp, /^v\s*(.+)$/
+      set :newest_version_url, 'http://etherpad.org/'
+      set :newest_version_selector, '#downloadbutton'
+      set :newest_version_regexp, /Version (.+)$/
 
       def project_url_for_version(version)
-         "https://github.com/Pita/etherpad-lite"
+         "https://github.com/ether/etherpad-lite/blob/master/CHANGELOG.md#v#{version.to_s.gsub('.', '')}"
       end
     end
   end
