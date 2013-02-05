b6d7ad1bde05ade52a5c6c61695ef0bb8698056e
Riyad Preukschas <riyad@informatik.uni-bremen.de>
2013-02-05 17:49:45 +0100

    Add Vanilla Forums detector

---
+++ lib/versionator/detector/vanilla.rb
@@ -0,0 +1,19 @@
+module Versionator
+  module Detector
+    # Detects {Vanilla}[http://vanillaforums.org/]
+    class Vanilla < Base
+      set :app_name, "Vanilla"
+      set :project_url, "http://vanillaforums.org/"
+
+      set :detect_dirs, %w{applications/vanilla conf js library themes uploads}
+      set :detect_files, %w{bootstrap.php index.php LICENSE.txt readme.html}
+
+      set :installed_version_file, "index.php"
+      set :installed_version_regexp, /^define\('APPLICATION_VERSION', '(.+)'\);$/
+
+      set :newest_version_url, 'http://vanillaforums.org/get/vanilla-core'
+      set :newest_version_selector, '#Body h1'
+      set :newest_version_regexp, /^Downloading: Vanilla version ([\d.]+)$/
+    end
+  end
+end
