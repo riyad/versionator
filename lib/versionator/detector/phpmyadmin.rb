5ca0ba40bba3d41cb5b23afea43b6d14bc4e4899
Riyad Preukschas <riyad@informatik.uni-bremen.de>
2013-02-15 16:50:53 +0100

    Make phpMyAdmin detector detect the upcoming 4.0

--- lib/versionator/detector/phpmyadmin.rb
+++ lib/versionator/detector/phpmyadmin.rb
@@ -7,7 +7,7 @@ module Versionator
       set :project_url, "http://www.phpmyadmin.net"
 
       set :detect_dirs, %w{js libraries themes}
-      set :detect_files, %w{export.php index.php main.php navigation.php README sql.php themes.php}
+      set :detect_files, %w{export.php index.php navigation.php README sql.php themes.php}
 
       set :installed_version_file, "README"
       set :installed_version_regexp, /^\s*Version (.+)$/
@@ -15,16 +15,6 @@ module Versionator
       set :newest_version_url, 'http://www.phpmyadmin.net/home_page/index.php'
       set :newest_version_selector, '#body .rightbuttons .downloadbutton .dlname a'
       set :newest_version_regexp, /^Download (.+)$/
-
-      def project_url_for_version(version)
-        if version < Versionomy.parse('3.3.9')
-          "http://sourceforge.net/projects/phpmyadmin/files/phpMyAdmin/#{version}/phpMyAdmin-#{version}-notes.html/view"
-        elsif Versionomy.parse('3.3.9') <= version && version < Versionomy.parse('3.4.7.1')
-          "http://sourceforge.net/projects/phpmyadmin/files/phpMyAdmin/#{version}/phpMyAdmin-#{version}.html/view"
-        else
-          "http://sourceforge.net/projects/phpmyadmin/files/phpMyAdmin/#{version}/phpMyAdmin-#{version}-notes.html/view"
-        end
-      end
     end
   end
 end
