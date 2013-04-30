fb1ba34841406d5339f712aa44fc87c7a26b9432
Riyad Preukschas <riyad@informatik.uni-bremen.de>
2013-04-30 13:31:06 +0200

    Fix OpenAtrium detector

--- lib/versionator/detector/open_atrium.rb
+++ lib/versionator/detector/open_atrium.rb
@@ -10,12 +10,12 @@ module Versionator
       set :detect_dirs, %w{includes misc modules profiles profiles/openatrium scripts sites themes}
       set :detect_files, %w{cron.php index.php install.php profiles/openatrium/VERSION.txt xmlrpc.php}
 
-      set :installed_version_file, "profiles/openatrium/VERSION.txt" # broken for 1.5
+      set :installed_version_file, "profiles/openatrium/VERSION.txt" # broken for 1.5 and 1.7
       set :installed_version_regexp, /^(.+)$/
 
       set :newest_version_url, 'http://openatrium.com/download'
       set :newest_version_selector, '#block-views-openatrium_releases-block_1 .field-content'
-      set :newest_version_regexp, /^Download (.+)$/
+      set :newest_version_regexp, /^Download(?: openatrium) (?:7.x-)?(.+)$/
     end
   end
 end
