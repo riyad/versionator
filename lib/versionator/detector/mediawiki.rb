896153d2bcdd5fab01b72e85ce836c4591b140c2
Riyad Preukschas <riyad@informatik.uni-bremen.de>
2013-05-29 22:18:02 +0200

    Update MediaWiki detector

--- lib/versionator/detector/mediawiki.rb
+++ lib/versionator/detector/mediawiki.rb
@@ -7,7 +7,7 @@ module Versionator
       set :project_url, "http://www.mediawiki.org"
       set :project_download_url, "http://www.mediawiki.org/wiki/Download"
 
-      set :detect_dirs, %w{bin extensions images includes maintenance skins}
+      set :detect_dirs, %w{extensions images includes maintenance skins}
       set :detect_files, %w{api.php includes/DefaultSettings.php index.php redirect.php thumb.php}
 
       set :installed_version_file, "includes/DefaultSettings.php" # see contents_detected?
