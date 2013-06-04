f8f7d27a4ecd25cba623d405735acd1c3d9bbccb
Riyad Preukschas <riyad@informatik.uni-bremen.de>
2013-06-04 12:53:55 +0200

    Update ReviewBoard detector

--- lib/versionator/detector/reviewboard.rb
+++ lib/versionator/detector/reviewboard.rb
@@ -14,8 +14,8 @@ module Versionator
       set :installed_version_regexp, /^Version: (.+)$/
 
       set :newest_version_url, 'http://www.reviewboard.org/'
-      set :newest_version_selector, '#latest_releases a'
-      set :newest_version_regexp, /^(.+)$/
+      set :newest_version_selector, '.top-downloads a'
+      set :newest_version_regexp, /^\s+Download Review Board v(.+)\s+$/
     end
   end
 end
