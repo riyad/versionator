eea88a0122b43c05c2bf2b422f4fc52c21e65693
Riyad Preukschas <riyad@informatik.uni-bremen.de>
2013-12-08 20:35:49 +0100

    Update Open Atrium detector

--- lib/versionator/detector/open_atrium.rb
+++ lib/versionator/detector/open_atrium.rb
@@ -5,7 +5,7 @@ module Versionator
     class OpenAtrium < Base
       set :app_name, "Open Atrium"
       set :project_url, "http://openatrium.com"
-      set :project_download_url, "http://openatrium.com/download"
+      set :project_download_url, "https://drupal.org/project/openatrium"
 
       set :detect_dirs, %w{includes misc modules profiles profiles/openatrium scripts sites themes}
       set :detect_files, %w{cron.php index.php install.php xmlrpc.php}
