804d23bebeb355808309b92c0f3b87e9cd883d28
Riyad Preukschas <riyad@informatik.uni-bremen.de>
2013-12-19 00:15:18 +0100

    Update Piwik detector

--- lib/versionator/detector/piwik.rb
+++ lib/versionator/detector/piwik.rb
@@ -5,17 +5,17 @@ module Versionator
     class Piwik < Base
       set :app_name, "Piwik"
       set :project_url, "http://piwik.org"
-      set :project_download_url, "http://piwik.org/download-piwik/"
+      set :project_download_url, "http://piwik.org/download/"
 
-      set :detect_dirs, %w{config core js libs misc plugins themes}
+      set :detect_dirs, %w{config core js libs misc plugins}
       set :detect_files, %w{core/Version.php index.php piwik.php}
 
       set :installed_version_file, "core/Version.php"
       set :installed_version_regexp, /^\s*const VERSION = '(.+)';$/
 
-      set :newest_version_url, 'http://piwik.org/'
-      set :newest_version_selector, '.download-button .title'
-      set :newest_version_regexp, /^\s*Download Piwik (.+)\s*$/
+      set :newest_version_url, 'http://piwik.org/download/'
+      set :newest_version_selector, '#startDownloadButton'
+      set :newest_version_regexp, /Download Piwik ([\d\.]+)/
     end
   end
 end
