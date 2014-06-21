c14e15b0025136b729d709749aa94a4dca78c24f
Riyad Preukschas <riyad@informatik.uni-bremen.de>
2014-06-21 15:08:31 +0200

    Add RainLoop detector

---
+++ lib/versionator/detector/rain_loop.rb
@@ -0,0 +1,20 @@
+module Versionator
+  module Detector
+    # Detects {RainLoop}[http://rainloop.net/].
+    class RainLoop < Base
+      set :app_name, "RainLoop"
+      set :project_url, "http://rainloop.net/"
+      set :project_download_url, "http://rainloop.net/downloads/"
+
+      set :detect_dirs, %w{data rainloop}
+      set :detect_files, %w{index.php data/VERSION}
+
+      set :installed_version_file, "data/VERSION"
+      set :installed_version_regexp, /([\d\.]+)/
+
+      set :newest_version_url, 'http://rainloop.net/downloads/'
+      set :newest_version_selector, '.container a.button'
+      set :newest_version_regexp, /Download - v([\d\.]+)/
+    end
+  end
+end
