2334d906f9637ea8474f93448cb7e2dcc38ef85f
Riyad Preukschas <riyad@informatik.uni-bremen.de>
2014-10-16 22:25:32 +0200

    Simplify OpenAtrium detector

--- lib/versionator/detector/open_atrium.rb
+++ lib/versionator/detector/open_atrium.rb
@@ -10,8 +10,8 @@ module Versionator
       set :detect_dirs, %w{includes misc modules profiles profiles/openatrium scripts sites themes}
       set :detect_files, %w{cron.php index.php install.php xmlrpc.php}
 
-      set :installed_version_file, "foo" # placeholder
-      set :installed_version_regexp, /^(foo)$/ # placeholder
+      set :installed_version_file, nil # placeholder
+      set :installed_version_regexp, nil # placeholder
 
       set :newest_version_url, 'https://drupal.org/project/openatrium'
       set :newest_version_selector, '.view-project-release-download-table .views-field-field-release-version a'
@@ -19,31 +19,23 @@ module Versionator
 
       # Overridden to take into account different files for different versions
       def detected?
-        super && (
-          File.readable?(File.join(base_dir, "profiles/openatrium/openatrium.info")) ||
-          File.readable?(File.join(base_dir, "profiles/openatrium/VERSION.txt"))
-        )
-      end
-
-      def version_2x?
-        File.readable?(File.join(base_dir, "profiles/openatrium/openatrium.info"))
+        super && installed_version_file
       end
 
       protected
 
       # Overridden to take into account different files for different versions
       def installed_version_file
-        if version_2x?
-          "profiles/openatrium/openatrium.info"
-        else
-          "profiles/openatrium/VERSION.txt"
-        end
+        %w{
+            profiles/openatrium/modules/contrib/oa_core/oa_core.info
+            profiles/openatrium/VERSION.txt
+        }.find{ |version_file| File.readable?(File.join(base_dir, version_file)) }
       end
 
       # Overridden to take into account different files for different versions
       def installed_version_regexp
-        if version_2x?
-          /^version = "7.x-(.+)"$/
+        if installed_version_file.end_with? ".info"
+          /^version = "\d.x-(.+)"$/
         else
           /^(.+)$/
         end
