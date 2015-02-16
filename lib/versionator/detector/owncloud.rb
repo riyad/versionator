574bdf1732f934e862a58ae4dc9fcdf16d5473c4
Riyad Preukschas <riyad@informatik.uni-bremen.de>
2015-02-16 21:06:48 +0100

    Update ownCloud detector

--- lib/versionator/detector/owncloud.rb
+++ lib/versionator/detector/owncloud.rb
@@ -4,8 +4,8 @@ module Versionator
     # Detects {ownCloud}[http://owncloud.org/].
     class Owncloud < Base
       set :app_name, "ownCloud"
-      set :project_url, "http://owncloud.org/"
-      set :project_download_url, "http://owncloud.org/install/"
+      set :project_url, "https://owncloud.org/"
+      set :project_download_url, "https://owncloud.org/install/"
 
       set :detect_dirs, %w{3rdparty apps core lib settings}
       set :detect_files, %w{db_structure.xml index.php version.php}
@@ -13,9 +13,9 @@ module Versionator
       set :installed_version_file, "version.php"
       set :installed_version_regexp, /\$OC_VersionString = '(.+)'/
 
-      set :newest_version_url, 'http://owncloud.org/install/'
+      set :newest_version_url, 'https://owncloud.org/install/'
       set :newest_version_selector, '.install .main p'
-      set :newest_version_regexp, /^Latest stable version: (7[\d\.]+)/
+      set :newest_version_regexp, /^Latest stable version: ([\d\.]+)/
 
       # Overridden to make sure that we do only detect the 5+ series
       def contents_detected?
