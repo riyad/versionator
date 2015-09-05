0d3d029f9d378962537ad7fe8e585bad171f3ec3
Riyad Preukschas <riyad@informatik.uni-bremen.de>
2015-09-05 23:57:33 +0200

    Update MediaWiki detector

--- lib/versionator/detector/mediawiki.rb
+++ lib/versionator/detector/mediawiki.rb
@@ -4,8 +4,8 @@ module Versionator
     # Detects {MediaWiki}[http://www.mediawiki.org].
     class Mediawiki < Base
       set :app_name, "MediaWiki"
-      set :project_url, "http://www.mediawiki.org"
-      set :project_download_url, "http://www.mediawiki.org/wiki/Download"
+      set :project_url, "https://www.mediawiki.org"
+      set :project_download_url, "https://www.mediawiki.org/wiki/Download"
 
       set :detect_dirs, %w{extensions images includes maintenance skins}
       set :detect_files, %w{api.php includes/DefaultSettings.php index.php thumb.php}
@@ -13,7 +13,7 @@ module Versionator
       set :installed_version_file, "includes/DefaultSettings.php" # see contents_detected?
       set :installed_version_regexp, /^\$wgVersion\s*= '(.+)';$/
 
-      set :newest_version_url, 'http://www.mediawiki.org/wiki/Download'
+      set :newest_version_url, 'https://www.mediawiki.org/wiki/Download'
       set :newest_version_selector, '#bodyContent .plainlinks a'
       set :newest_version_regexp, /^Download MediaWiki (.+)$/
     end
