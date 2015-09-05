e27f899b53b4035cfd8f304733fec4f4d64f09db
Riyad Preukschas <riyad@informatik.uni-bremen.de>
2015-09-05 23:58:44 +0200

    Update Trac detectors

--- lib/versionator/detector/trac_current.rb
+++ lib/versionator/detector/trac_current.rb
@@ -7,19 +7,15 @@ module Versionator
       set :project_url, "http://trac.edgewall.org"
       set :project_download_url, "http://trac.edgewall.org/wiki/TracDownload#LatestStableRelease"
 
-      set :detect_dirs, %w{contrib doc trac tracopt}
-      set :detect_files, %w{PKG-INFO setup.cfg setup.py}
+      set :detect_dirs, %w{contrib doc trac}
+      set :detect_files, %w{PKG-INFO setup.cfg setup.py tracini.cfg}
 
       set :installed_version_file, "PKG-INFO"
-      set :installed_version_regexp, /^Version: (.+)$/
+      set :installed_version_regexp, /^Version: (1\.[\d\.]+)$/
 
       set :newest_version_url, 'http://trac.edgewall.org/wiki/TracDownload'
       set :newest_version_selector, '#LatestStableRelease'
-      set :newest_version_regexp, /^.+ - Trac (.+)$/
-
-      def contents_detected?
-        super && installed_version.major > 0
-      end
+      set :newest_version_regexp, /^.+ - Trac (1\.[\d\.]+)$/
     end
   end
 end
--- lib/versionator/detector/trac_old.rb
+++ lib/versionator/detector/trac_old.rb
@@ -7,19 +7,15 @@ module Versionator
       set :project_url, "http://trac.edgewall.org"
       set :project_download_url, "http://trac.edgewall.org/wiki/TracDownload#PreviousStableRelease"
 
-      set :detect_dirs, %w{contrib doc trac}
+      set :detect_dirs, %w{contrib doc trac wiki-macros}
       set :detect_files, %w{PKG-INFO setup.cfg setup.py}
 
       set :installed_version_file, "PKG-INFO"
-      set :installed_version_regexp, /^Version: (.+)$/
+      set :installed_version_regexp, /^Version: (0\.[\d\.]+)$/
 
       set :newest_version_url, 'http://trac.edgewall.org/wiki/TracDownload'
       set :newest_version_selector, '#PreviousStableRelease'
-      set :newest_version_regexp, /^.+ - Trac (.+)$/
-
-      def contents_detected?
-        super && installed_version.major == 0
-      end
+      set :newest_version_regexp, /^.+ - Trac (0\.[\d\.]+)$/
     end
   end
 end
