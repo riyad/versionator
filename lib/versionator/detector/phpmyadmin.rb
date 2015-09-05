090eb1d35781a1545bc2c4ca5089777512ef4296
Riyad Preukschas <riyad@informatik.uni-bremen.de>
2015-09-05 23:57:43 +0200

    Update phpMyAdmin detector

--- lib/versionator/detector/phpmyadmin.rb
+++ lib/versionator/detector/phpmyadmin.rb
@@ -4,7 +4,7 @@ module Versionator
     # Detects {phpMyAdmin}[http://www.phpmyadmin.net].
     class Phpmyadmin < Base
       set :app_name, "phpMyAdmin"
-      set :project_url, "http://www.phpmyadmin.net"
+      set :project_url, "https://www.phpmyadmin.net"
       set :project_download_url, "http://www.phpmyadmin.net/home_page/downloads.php"
 
       set :detect_dirs, %w{js libraries themes}
@@ -13,9 +13,9 @@ module Versionator
       set :installed_version_file, "README"
       set :installed_version_regexp, /^\s*Version (.+)$/
 
-      set :newest_version_url, 'http://www.phpmyadmin.net/home_page/index.php'
+      set :newest_version_url, 'https://www.phpmyadmin.net/'
       set :newest_version_selector, '#body .downloadbutton a'
-      set :newest_version_regexp, /^Download (.+)$/
+      set :newest_version_regexp, /^\s*Download (.+)$/
     end
   end
 end
